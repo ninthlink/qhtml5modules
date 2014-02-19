61b70ff71597c7208fc7d99c40f7703a794bb66b
alex chousmith <alex@ninthlink.com>
2014-02-19 04:07:20 +0000

    additional mobile viewport metas try?

--- modules/who-is-qualcomm/index.php
+++ modules/who-is-qualcomm/index.php
@@ -16,8 +16,10 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<title>Who is Qualcomm?</title>
-		<meta name="description" content="">
-		<meta name="viewport" content="width=device-width">
+		
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
+		<meta name="viewport" content="width=device-width, target-densitydpi=160dpi, initial-scale=1.0">
 
 		<link rel="stylesheet" href="css/main.css">
 
