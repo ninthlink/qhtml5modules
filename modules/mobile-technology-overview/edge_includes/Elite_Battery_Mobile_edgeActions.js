7116015a1437da91c4b08f24bfc6f08022522b55
alex chousmith <alex@ninthlink.com>
2014-02-20 18:04:58 +0000

    section #3 & dont need those other 2 html files now that i am figuring this piecing out

---
+++ modules/mobile-technology-overview/edge_includes/Elite_Battery_Mobile_edgeActions.js
@@ -0,0 +1,12 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'Person'
+(function(symbolName){})("Person");
+//Edge symbol end:'Person'
+})(jQuery,AdobeEdge,"EDGE-58030357");
