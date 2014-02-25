7e96623a97bf6e01a636a33bd4b934eab4c4c5ce
alex chousmith <alex@ninthlink.com>
2014-02-25 20:11:40 +0000

    "* as compared to devices without fast charge technology" but still no loader for this one?

--- modules/5coolthings/index.php
+++ modules/5coolthings/index.php
@@ -3,7 +3,7 @@
  * Qualcomm HTML5 Modules
  * 5 Cool Thing
  *
- * v0.1.7
+ * v0.1.8
  */
 ?>
 <!doctype html>
@@ -11,33 +11,29 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<title>5 Cool Things</title>
-		<meta name="description" content="">
-		<meta name="viewport" content="width=device-width">
+		
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-touch-fullscreen" content="yes" />
+		<meta name="viewport" content="width=device-width, target-densitydpi=160dpi, initial-scale=1.0, minimum-scale=1.0, user-scalable=no">
 
+		<?php /*
+		<link rel="stylesheet" href="../_common/css/jquery.qualcomm-reveal.css">
+		*/ ?>
 		<link rel="stylesheet" href="css/main.css">
 
 		<script src="../_common/js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
 		<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script><?php /* load CDN or local backup */ ?>
-		<script>window.jQuery || document.write('<script src="../_common/js/jquery-1.10.1.min.js"><\/script>')</script>
-
-		<script src="../_common/js/spin.min.js"></script>
-		<script src="../_common/js/jquery.scrollorama.js"></script>
-		<script src="../_common/js/waypoints.min.js"></script>
-		<script src="../_common/js/swiper.min.js"></script>
-
 	</head>
 
 	<body>
-		
 		<div id="wrapper">
-
-			<?php // Slide #1 ?>
+			<?php // Slide #0 ?>
 			<header class="bg-red-grad">
 				<div class="inner">
 					<h1 class="txtw"><strong>5</strong><span class="cool">Super cool things<br /><span class="thin">about Qualcomm<sup>&reg;</sup><br />Snapdragon<sup>&trade;</sup> Processors</span></span></h1>
@@ -59,7 +55,7 @@
 							<div class="phone"></div>
 						</div>
 						<div class="text-position">
-							<h3 class="center">Can make your phone do <br />things <span class="thick">by talking to it</span> <sup class="sup">1</sup></h3>
+							<h3 class="center">Can make your phone do <br />things <span class="thick">by talking to it</span></h3>
 						</div>
 
 						<span class="stretch"></span>
@@ -75,7 +71,7 @@
               </div>
 						</div>
 						<div class="text-position">
-							<h3 class="center">Can charge devices <span>up to <span class="thick">75% faster</span> <sup class="sup">2</sup></span></h3>
+							<h3 class="center">Can charge devices <span>up to <span class="thick">75% faster</span> <sup class="sup">*</sup></span></h3>
 						</div>
 
 						<span class="stretch"></span>
@@ -93,7 +89,7 @@
               </div>
 						</div>
 						<div class="text-position">
-							<h3 class="center">Can access <span class="thick">50 satellites</span> <sup class="sup">3</sup></h3>
+							<h3 class="center">Can access <span class="thick">50 satellites</span></h3>
 						</div>
 
 						<span class="stretch"></span>
@@ -110,7 +106,7 @@
               </div>
 						</div>
 						<div class="text-position">
-							<h3 class="center">Stays cool under pressure<br /><span class="thick">battery efficient</span> <sup class="sup">4</sup></h3>
+							<h3 class="center">Stays cool under pressure<br /><span class="thick">battery efficient</span></h3>
 						</div>
 
 						<span class="stretch"></span>
@@ -126,7 +122,7 @@
 							</div>
 						</div>
 						<div class="text-position">
-							<h3 class="center">High performance <span class="thick">graphics</span><br /><span class="l2">with <span class="thick">lower power</span> usage <sup class="sup">5</sup></span></h3>
+							<h3 class="center">High performance <span class="thick">graphics</span><br /><span class="l2">with <span class="thick">lower power</span> usage</span></h3>
 						</div>
 
 						<span class="stretch"></span>
@@ -134,30 +130,33 @@
 				</section>
         
 				<?php // Slide #6 ?>
-				<section id="legal" class="bg-red">
+				<section id="legal" class="bg-red block">
 					<div class="inner">
 						<div class="graphic-position center">
 							<img id="logo" src="img/snapdragon.png" />
 						</div>
 						<div class="text-position center txtw">
 							<p>&copy; <?php date_default_timezone_set('UTC'); echo date('Y'); ?> Qualcomm Technologies, Inc.  All rights reserved.</p>
-              <p>1 - Nsectetur adipisicing elite sed do. Eiusmod tempor incididunt ut.<br />
-              2 - Nsectetur adipisicing elite sed do. Eiusmod tempor incididunt ut.<br />
-              3 - Nsectetur adipisicing elite sed do. Eiusmod tempor incididunt ut.<br />
-              4 - Nsectetur adipisicing elite sed do. Eiusmod tempor incididunt ut.<br />
-              5 - Nsectetur adipisicing elite sed do. Eiusmod tempor incididunt ut.</p>
+              <p>* As compared to devices without fast charge technology.</p>
 							<p>Qualcomm is a trademark of QUALCOMM Incorporated registered in the United States and other countries. Gobi and Snapdragon are trademarks of Qualcomm Technologies, Inc.<br />Other products and brand names are property of their respective owners.</p>
 						</div>
 					</div>
 				</section>
 				
 			</div>
-
-			<footer>
-			</footer>
 	 
 		</div>
 
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script><?php /* load CDN or local backup */ ?>
+		<script>window.jQuery || document.write('<script src="../_common/js/jquery-1.10.1.min.js"><\/script>')</script>
+
+		<script src="../_common/js/spin.min.js"></script>
+		<?php /*
+		<script src="../_common/js/jquery.inview.min.js"></script>
+		<script src="../_common/js/jquery.qualcomm-reveal.min.js"></script>
+		*/?>
+		<script src="../_common/js/waypoints.min.js"></script>
+		
 		<script src="js/animate.js"></script>
 <script type="text/javascript"> 
 //browser-update.org
