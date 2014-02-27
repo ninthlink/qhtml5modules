5aedb74aec47cb90f387b26c7543db7b29e732ad
alex chousmith <alex@ninthlink.com>
2014-02-27 22:14:08 +0000

    footer disclaimer

--- modules/6ways/index.php
+++ modules/6ways/index.php
@@ -2,7 +2,7 @@
 /*
  * Qualcomm HTML5 Modules : 6 ways smartphones are changing us
  */
-$v = '0.1.3';
+$v = '0.1.4';
 $qv = '?v='. $v;
 ?>
 <!doctype html>
@@ -106,7 +106,7 @@ $qv = '?v='. $v;
 				<div class="inner">
 					<p><img src="img/qualcomm.png" alt="Qualcomm" /></p>
 				<p>&copy; <?php date_default_timezone_set('UTC'); echo date('Y'); ?> Qualcomm Technologies, Inc. All rights reserved.</p>
-				<p>Sources: Facebook, Jan. '13; SA, Apr. '12; 2012 Snapdragon Consumer Survey;<br />TIME Mobility Poll, in cooperation with QUALCOMM, Aug. '12</p>
+				<p>Sources: Facebook, Jan. '13; SA, Apr. '12; 2012 Snapdragon Consumer Survey; TIME Mobility Poll, in cooperation with QUALCOMM, Aug. '12</p>
 <p>Qualcomm is a trademark of QUALCOMM Incorporated registered in the United States and other countries. Gobi and Snapdragon are trademarks of Qualcomm Technologies, Inc. Other products and brand names are property of their respective owners.</p>
 				</div>
 			</div>
