cc06ea26b069bcbf2b0036ce8539effa9f233497
alex chousmith <alex@ninthlink.com>
2014-04-04 06:01:46 +0000

    connectivity module, blocks 7-19

---
+++ modules/connectivity/index.php
@@ -0,0 +1,259 @@
+<?php
+/*
+ * Qualcomm HTML5 Modules : Connectivity
+ */
+$v = '0.1.0';
+$qv = '?v='. $v;
+?>
+<!doctype html>
+<html class="no-js">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Connectivity</title>
+		
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-touch-fullscreen" content="yes" />
+		<meta name="viewport" content="width=device-width, target-densitydpi=160dpi, initial-scale=1.0, minimum-scale=1.0, user-scalable=no">
+
+		<link rel="stylesheet" href="../_common/css/jquery.qualcomm-reveal.css">
+		<link rel="stylesheet" href="css/main.css<?php echo $qv ?>">
+		
+		<script src="../_common/js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+		<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+		<script type="text/javascript" src="//cdn.sublimevideo.net/js/brxysbmu.js"></script>
+	</head>
+	<body class="bg-grey">
+			<div id="content" class="content-container">
+				
+				<?php // Slide #7 ?>
+				<section id="check" class="bg-img block comeon noscale">
+					<div class="inner">
+						<div class="graphic-position">
+							<div class="eco growin"><div class="in"></div></div>
+						</div>
+						<div class="text-position txtw">
+							<h2 class="fadein">Let's <span class="thick">check out</span> the <br class="vis960" />Snapdragon modem</h2>
+							<p class="fadein">Qualcomm Technologies was the first to integrate application processors with 3G/4G LTE to ensure a rich seamless user experience.</p>
+						</div>
+					</div>
+				</section>
+				
+				<?php // Slide #8 ?>
+				<section id="chipd" class="bg-grey-grad block comeon">
+					<div class="inner">
+						<div class="text-position center">
+							<h2>Qualcomm Technologies <span class="thick">custom designs</span> <br class="vis960" />its own modem for integration into <br class="vis960" />Snapdragon processors</h2>
+							<p>Provides a complete solution including modem and RF for optimized performance</p>
+						</div>
+						<div class="graphic-position">
+							<div class="eco chip growin"><div class="in"></div></div>
+						</div>
+					</div>
+				</section>
+				
+				<?php // Slide #9 ?>
+				<section id="super" class="bg-white block comeon">
+					<div class="inner">
+						<div class="text-position center">
+							<h2>A modem that offers a <span class="thick">superior user experience</span></h2>
+						</div>
+						<div class="graphic-position">
+							<div class="phone"></div>
+							<a href="#prev" class="btn prev fadein"></a>
+							<a href="#prev" class="btn next fadein"></a>
+						</div>
+					</div>
+				</section>
+				
+				<?php // Slide #9.5 img ?>
+				<section id="couple" class="bg-img block" data-bg-height="481" data-min-height="360"></section>
+				
+				<?php // Slide #10 ?>
+				<section id="benies" class="bg-grey-grad block comeon" data-waypoint-offset="50%">
+					<div class="inner">
+						<div class="text-position center">
+							<h2>The modem is designed to drive <br class="vis960" /><span class="thick">benefits</span> to end users</h2>
+							<p>Improved performance, power, mobility, and coverage enhances mobile experiences</p>
+						</div>
+						<div class="graphic-position">
+							<div class="bk fast">
+								<div class="eco"><div class="in"></div></div>
+								<h3>Faster Connectivity</h3>
+								<p>Exceptional streaming, surfing,<br />uploads, and downloads</p>
+							</div>
+							<div class="bk call">
+								<div class="eco"><div class="in"></div></div>
+								<h3>Seamless Calls and Connectivity</h3>
+								<p>Talking and browsing without interruption</p>
+							</div>
+							<div class="bk perf">
+								<div class="eco"><div class="in"></div></div>
+								<h3>Reliable Performance</h3>
+								<p>Outstanding device connectivity</p>
+							</div>
+							<div class="bk batt">
+								<div class="eco"><div class="in"></div></div>
+								<h3>Battery Efficiency</h3>
+								<p>The integrated modem with advanced technologies helps to extend battery life</p>
+							</div>
+						</div>
+						
+						<span class="stretch"></span>
+					</div>
+				</section>
+				
+				<?php // Slide #11 ?>
+				<section id="delivr" class="bg-red-grad-lt block comeon" data-waypoint-offset="30%">
+					<div class="inner">
+						<div class="text-position txtw center">
+							<h2>QUALCOMM Technologies 3G modem is <br class="vis960" /><span class="thick">designed to deliver</span> incredible performance</h2>
+						</div>
+						<div class="graphic-position">
+							<div class="eco faster">
+								<div class="in"></div>
+								<div class="lbl txtw">Faster Web<br />Browsing</div>
+							</div>
+							<div class="eco line l1"><div class="in"></div></div>
+							<div class="eco video">
+								<div class="in"></div>
+								<div class="lbl txtw">Uninterrupted<br />video streaming</div>
+							</div>
+							<div class="eco line l2"><div class="in"></div></div>
+							<div class="eco longer">
+								<div class="in"></div>
+								<div class="lbl txtw">Longer Talk<br />Times</div>
+							</div>
+						</div>
+						<span class="stretch"></span>
+					</div>
+				</section>
+				
+				<?php // Slide #12 ?>
+				<section id="vs3g" class="bg-6 block vid">
+					<div class="inner">
+						<div class="text-position">
+							<h2 class="center txtw">3G vs. 3G</h2>
+							<div class="videohere">
+							<video id="v3gvs" poster="img/vid-placeholder.png" width="600" height="338" title="3G vs. 3G" data-uid="v3gvs" data-autoresize="fit" preload="none">
+							  <source src="video/evolution_of_a_name_cutdown_640x360.mp4" />
+							</video>
+							</div>
+						</div>
+
+						<span class="stretch"></span>
+					</div>
+				</section>
+			
+				<?php // Slide #13 ?>
+				<section id="adv4g" class="bg-white block comeon">
+					<div class="inner">
+						<div class="text-position center">
+							<h2>Qualcomm Technologies <span class="thick">4G LTE advantages</span></h2>
+							<p>Our 4G modems perform on features, speed, size and power</p>
+						</div>
+						<div class="graphic-position">
+							<div class="eco faster">
+								<div class="in"></div>
+								<div class="lbl">Faster Speeds</div>
+							</div>
+							<div class="eco line l1"><div class="in"></div></div>
+							<div class="eco cooler">
+								<div class="in"></div>
+								<div class="lbl">Cooler Devices</div>
+							</div>
+							<div class="eco line l2"><div class="in"></div></div>
+							<div class="eco power">
+								<div class="in"></div>
+								<div class="lbl">Lower Power</div>
+							</div>
+						</div>
+						<span class="stretch"></span>
+					</div>
+				</section>
+				
+				<?php // Slide #14 ?>
+				<section id="vs4g" class="bg-6 block vid">
+					<div class="inner">
+						<div class="text-position">
+							<h2 class="center txtw">4G vs. 4G</h2>
+							<div class="videohere">
+							<video id="v4gvs" poster="img/vid-placeholder.png" width="600" height="338" title="4G vs. 4G" data-uid="v4gvs" data-autoresize="fit" preload="none">
+							  <source src="video/evolution_of_a_name_cutdown_640x360.mp4" />
+							</video>
+							</div>
+						</div>
+
+						<span class="stretch"></span>
+					</div>
+				</section>
+				
+				<?php // Slide #14.5 img ?>
+				<section id="warrowd" class="bg-img block" data-bg-height="497" data-min-height="360"></section>
+        
+				<?php // Slide #15 ?>
+				<section id="leader" class="bg-red-grad-lt block comeon" data-waypoint-offset="50%">
+					<div class="inner">
+          	<div class="graphic-position">
+							<div class="eco fadein"><div class="in"></div></div>
+            </div>
+						<div class="text-position txtw">
+							<h2>Qualcomm&reg; technology<br /><span class="thick">leadership</span> in 3G/4G LTE</h2>
+							<p>Introduced 4th generation World Mode LTE modem with peak download data rates of 300 Mbps while most competitors were still working on their first generation products</p>
+              <ul>
+                <li class="fadein l0">4th generation LTE modem</li>
+                <li class="fadein l1">Multimode and multiband support</li>
+                <li class="fadein l2">First with LTE Advanced</li>
+              </ul>
+						</div>
+
+						<span class="stretch"></span>
+					</div>
+				</section>
+				
+				<?php // quiz CTA ?>
+        <section id="proceed" class="bg-white block">
+        	<div class="inner">
+          	<div class="text-position center">
+            	<div class="video-button">
+								<h2><a href="assessment.html" class="txtbk" onClick="parent.qelite_taketraining_btn(); return false;">Proceed to quiz <span class="sprite play"></span></a></h2>
+							</div>
+            </div>
+          </div>
+        </section>
+        
+				<?php // Slide #20 ?>
+				<section id="legal" class="bg-6 block">
+					<div class="inner">
+						<div class="text-position txtw">
+							<h3>Footnotes</h3>
+							<ol>
+							<li>lorem ipsum dolor</li>
+							<li>Source: lorem ipsum, 2013</li>
+							</ol>
+							<hr />
+							<p>&copy;<?php date_default_timezone_set('UTC'); echo date('Y'); ?> Qualcomm Incorporated and/or its subsidiaries. All Rights Reserved.</p>
+							<p>References to "Qualcomm" may mean Qualcomm Incorporated, Qualcomm Technologies, Inc. or subsidiaries or business units within the Qualcomm corporate structure, as applicable.</p>
+							<p>Qualcomm Incorporated includes Qualcomm's licensing business, QTL, and the vast majority of its patent portfolio. Qualcomm Technologies, Inc., a wholly-owned subsidiary of Qualcomm Incorporated, operates, along with its subsidiaries, substantially all of Qualcomm's engineering, research and development functions, and substantially all of its products and services businesses, including its semiconductor business, QCT.</p>
+							<p>Nothing in these materials is an offer to sell any of the components or devices referenced herein.</p>
+						</div>
+					</div>
+				</section>
+				
+			</div>
+
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="../_common/js/jquery-1.10.1.min.js"><\/script>')</script>
+
+		<script src="../_common/js/spin.min.js"></script>
+		<script src="../_common/js/jquery.inview.min.js"></script>
+		<script src="../_common/js/jquery.qualcomm-reveal.min.js?v=1.0.3"></script>
+
+		<script src="../_common/js/waypoints.min.js"></script>
+		<script src="../_common/js/jquery.touchSwipe.min.js"></script>
+		
+		<script src="js/animate.js<?php echo $qv ?>"></script>
+	</body>
+</html>
---
+++ modules/connectivity/js/animate.js
@@ -0,0 +1,102 @@
+/**
+ * Javascript Animations & Helpers
+**/
+$(document).ready(function() {
+	// default animation offset for each section (variance from top of <section>)
+	var qwh, qww, offsetDefault = '30%';
+	// scale full bg img slides
+	$('.bg-img').each(function() {
+		var th = $(this).height();
+		var mh = $(this).data('min-height');
+		if ( mh ) th = mh;
+		$(this).data('og-height', th);
+	});
+	$(window).bind('resize', function() {
+		qwh= $(this).height();
+		qww = $(this).width();
+		$('.bg-img:not(.noscale)').each(function() {
+			var ogh = $(this).data('og-height');
+			var bgh = $(this).data('bg-height');
+			$(this).height(function( index, height ) {
+				//if ( $(this).hasClass('max-height') ) return qwh;
+				
+				var nh = Math.round(qww * bgh / 1280);
+				if ( $(this).hasClass('max-height') ) {
+					if ( nh > qwh ) nh = qwh;
+				} else if ( nh < ogh ) {
+					nh = ogh;
+				}
+				return nh;
+			});
+		});
+	}).trigger('resize');
+	
+	// "qualcommreveal" loader
+	$('.content-container').qualcommreveal({
+      targets: '.block',
+	  scrollspeed: 0,
+    });
+	$.subscribe('qualcommreveal/reveal-finish', function(event, elem, counter) {
+		elem.trigger('qinview');
+	});
+	
+	// waypoints & more mayhem
+	var onoffsets = new Array();
+	$('.comeon').each(function(i) {
+		if ( $(this).is('[data-waypoint-offset]') == false ) {
+			$(this).data('waypoint-offset', offsetDefault);
+		}
+		var ofs = $(this).data('waypoint-offset');
+		onoffsets[i] = ofs;
+		$(this).on('qinview', function() {
+			$(this).waypoint(function() {
+				$(this).addClass('onn');
+			}, {
+				offset: onoffsets[i]
+			});
+		}).on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+			if(!isInView) {
+				$(this).trigger('reset');
+			}
+		});
+	});
+	// #10 benies
+	$('#benies .bk').children().addClass('fadein');
+	// #11 delivr
+	$('#delivr .graphic-position').children().addClass('fadein');
+	// # videos
+	$('#vs3g').bind('qinview', function() {
+		sublime.prepare('v3gvs');
+	});
+	$('#vs4g').bind('qinview', function() {
+		sublime.prepare('v4gvs');
+	});
+	// #13 adv4g
+	$('#adv4g .graphic-position').children().addClass('fadein');
+	
+	// add reset btn to each block?
+	$('.block').append('<a href="#" class="btn-reset"><i></i></a>').children('.btn-reset').click(function() {
+		var bpid = '#' + $(this).parent().removeClass('onn').attr('id');
+		setTimeout(function() {
+			$(bpid).addClass('onn').trigger('reset');
+		}, 0);
+		return false;
+	});
+	// fun with ecos
+	$('.eco:not(.bg)').click(function() {
+		$(this).fadeOut(100,function() { $(this).fadeIn(1000) });
+	});
+});
+
+//browser-update.org
+var $buoop = {vs:{i:9,f:5,o:12,s:5,n:9},reminder:0,onshow:function(infos) {
+	jQuery('#buorg > div').prepend('<strong>This module is built with the latest HTML5 + CSS3 in mind.</strong> ');
+	}}; 
+$buoop.ol = window.onload; 
+window.onload=function(){ 
+ try {if ($buoop.ol) $buoop.ol();}catch (e) {} 
+ var e = document.createElement("script"); 
+ e.setAttribute("type", "text/javascript"); 
+ e.setAttribute("src", "//browser-update.org/update.js"); 
+ document.body.appendChild(e); 
+} 
